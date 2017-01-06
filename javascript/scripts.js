53f5ff87f04cdf4bb96e78b2457d7302b3b064c8
aaron.harpt <aaron.harpt@gmail.com>
2017-01-06 17:22:29 -0600

    Worked on login enabled button form

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,15 @@
-$(".btn-primary").click(function () {
+// when the button is clicked
+  // if email is not longer than one character throw an error
+  // if email is not formatted like an email throw an error
+  // if Password if not longer one character throw an error
+
+
+$(".btn-default").click(function () {
   "use strict";
-  if ($("label").val().length < 1) {
-    $(".form-group").addClass("has-success");
+  if ($("#email-label").val().length < 1) {
+    $("#email-label").parent(".form-group").addClass("has-error");
+    $("#email-label").next(".col-sm-10").append("<span class='error-message'>Your provided email is not the required length. Please try again.</span>");
+    console.log("#email-label length less than 1");
   }
+  console.log("button default clicked");
 });
