6a5929961a71d232b3ccbac422241af57564fe7c
aaron.harpt <aaron.harpt@gmail.com>
2017-01-07 11:34:46 -0600

    Worked on password form

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,7 @@
 // when the button is clicked
   // if email is not longer than one character throw an error
   // if email is not formatted like an email throw an error
-  // if Password if not longer one character throw an error
+  // if Password if not longer than six characters throw an error
 
 // if email is not longer than one character throw an error
 
@@ -64,3 +64,29 @@ $(".btn-default").click(function () {
 //       }
 //     }
 //   });
+
+
+// if Password if not longer than six characters throw an error
+
+$(".btn-default").click(function () {
+  "use strict";
+  if ($("#inputPassword3").val().length < 6) {
+    $("#password-label").parent(".form-group").addClass("has-error");
+    $("#password-label").next(".col-sm-10").append("</span><span class='error-message'>Please type in a password that is longer than five characters.</span>");
+    console.log("#password-label length less than 1");
+  }
+  console.log("button default for password input is clicked");
+});
+
+
+  $("#inputPassword3").keyup( function () {
+    "use strict";
+    if ($("#password-label").parent(".form-group").hasClass("has-error")) {
+      if ($("#inputPassword3").val().length > 5) {
+        $("#password-label").parent(".form-group").removeClass("has-error");
+        $("#password-label").parent(".form-group").addClass("has-success");
+        $("#password-label").next(".col-sm-10").children(".error-message").remove();
+        console.log("keyup removing has-error class and adding has-success for password. Also removing .error-message");
+      }
+    }
+  });
