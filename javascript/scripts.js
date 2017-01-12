95c59998ddaa430feef40b3075556e49840c4c96
aaron.harpt <aaron.harpt@gmail.com>
2017-01-11 22:58:57 -0600

    Worked on making code consistent throughout file

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -30,14 +30,14 @@ $(".btn-default").click(function (event) {
       if ($("#inputEmail3").val().length < 1 && !re.test($("#inputEmail3").val())) {
         event.preventDefault();
         $("#email-label").parent(".form-group").removeClass("has-error has-success").addClass("has-error");
-        $("#email-label").next(".col-sm-10").children("span").remove();
+        $("#email-label").next(".col-sm-10").children(".error-message").remove();
         $("#email-label").next(".col-sm-10").append("<span class='error-message'>Your provided email is not the required length.</span>");
         console.log("#email-label length less than 1 character on keyup");
 
       } else if ($("#inputEmail3").val().length > 0 && !re.test($("#inputEmail3").val())) {
         event.preventDefault();
         $("#email-label").parent(".form-group").removeClass("has-error has-success").addClass("has-error");
-        $("#email-label").next(".col-sm-10").children("span").remove();
+        $("#email-label").next(".col-sm-10").children(".error-message").remove();
         $("#email-label").next(".col-sm-10").append("<span class='error-message'>Your provided email is not formatted like an email.</span>");
         console.log("keyup changing error-message text");
       } else if ($("#inputEmail3").val().length > 0 && re.test($("#inputEmail3").val())) {
