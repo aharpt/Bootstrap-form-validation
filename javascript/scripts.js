6c3eff2bce55192d36685ef49fdc289bd4cfd5a2
aaron.harpt <aaron.harpt@gmail.com>
2017-01-11 20:08:42 -0600

    Fixed password field

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -66,20 +66,18 @@ $(".btn-default").click(function (event) {
 
   $("#inputPassword3").keyup( function (event) {
     "use strict";
-    if ($("#password-label").parent(".form-group").hasClass("has-error")) {
-      if ($("#inputPassword3").val().length < 6) {
-        event.preventDefault();
-        $("#password-label").parent(".form-group").addClass("has-error");
-        $("#password-label").next(".col-sm-10").children(".error-message").remove();
-        $("#password-label").next(".col-sm-10").append("</span><span class='error-message'>Please type in a password that is longer than five characters.</span>");
-        console.log("#password-label length less than 6 characters for keyup");
-      }
-      if ($("#inputPassword3").val().length > 5) {
-        $("#password-label").parent(".form-group").removeClass("has-error");
-        $("#password-label").parent(".form-group").addClass("has-success");
-        $("#password-label").next(".col-sm-10").children(".error-message").remove();
-        console.log("keyup removing has-error class and adding has-success for password. Also removing .error-message");
-      }
+    if ($("#inputPassword3").val().length < 6) {
+      event.preventDefault();
+      $("#password-label").parent(".form-group").addClass("has-error");
+      $("#password-label").next(".col-sm-10").children(".error-message").remove();
+      $("#password-label").next(".col-sm-10").append("</span><span class='error-message'>Please type in a password that is longer than five characters.</span>");
+      console.log("#password-label length less than 6 characters for keyup");
+    }
+    if ($("#inputPassword3").val().length > 5) {
+      $("#password-label").parent(".form-group").removeClass("has-error has-success");
+      $("#password-label").parent(".form-group").addClass("has-success");
+      $("#password-label").next(".col-sm-10").children(".error-message").remove();
+      console.log("keyup removing has-error class and adding has-success for password. Also removing .error-message");
     }
   });
 
