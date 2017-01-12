2326bd57f6b199946923b5f6192e3ef8147e7a98
aaron.harpt <aaron.harpt@gmail.com>
2017-01-11 23:02:07 -0600

    Updated "jslint" gulp task gulp.src file path

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var jslint = require("gulp-jslint");
 
 
 gulp.task("jslint", function () {
-  return gulp.src("./script.js")
+  return gulp.src("./javascript/script.js")
   .pipe(jslint({
     browser: true,
     devel: true,
