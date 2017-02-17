d1dd9e289bf0c6d879f6fdf982c1d2cf3e2b8a66
aaron.harpt <aaron.harpt@gmail.com>
2017-02-17 14:17:16 -0600

    Refactored login form jQuery code

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -11,57 +11,63 @@ var numOfClicks = 0;
 
 $(function() {
 
+  var $btn = $(".btn");
+  var $inputEmail = $("#inputEmail");
+  var $emailLabel = $("#email-label");
+  var $inputPassword = $("#inputPassword");
+  var $passwordLabel = $("#password-label");
 
-$(".btn").click( function (event) {
+
+$btn.click( function (event) {
   "use strict";
-  if (!re.test($("#inputEmail").val())) {
+  if (!re.test($inputEmail.val())) {
     event.preventDefault();
-    $("#email-label").parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-error has-feedback");
-    $("#email-label").next(".col-sm-10").children(".error-message").remove();
-    $("#email-label").next(".col-sm-10").append("<span class='glyphicon glyphicon-remove form-control-feedback' aria-hidden='true'></span>");
-    $("#email-label").next(".col-sm-10").append("<span class='error-message'>Please provide a valid email</span>");
+    $emailLabel.parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-error has-feedback");
+    $emailLabel.next(".col-sm-10").children(".error-message").remove();
+    $emailLabel.next(".col-sm-10").append("<span class='glyphicon glyphicon-remove form-control-feedback' aria-hidden='true'></span>");
+    $emailLabel.next(".col-sm-10").append("<span class='error-message'>Please provide a valid email</span>");
   }
 });
 
-  $("#inputEmail").blur( function () {
-    if ($("#inputEmail").val().length > 0 && re.test($("#inputEmail").val())) {
-      $("#email-label").parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-success has-feedback");
-      $("#email-label").next(".col-sm-10").children("span").remove();
-      $("#email-label").next(".col-sm-10").append("<span class='glyphicon glyphicon-ok form-control-feedback' aria-hidden='true'></span>");
+  $inputEmail.blur( function () {
+    if ($inputEmail.val().length > 0 && re.test($inputEmail.val())) {
+      $emailLabel.parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-success has-feedback");
+      $emailLabel.next(".col-sm-10").children("span").remove();
+      $emailLabel.next(".col-sm-10").append("<span class='glyphicon glyphicon-ok form-control-feedback' aria-hidden='true'></span>");
     } else {
-      $("#email-label").parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-error has-feedback");
-      $("#email-label").next(".col-sm-10").children("span").remove();
-      $("#email-label").next(".col-sm-10").append("<span class='glyphicon glyphicon-remove form-control-feedback' aria-hidden='true'></span>");
-      $("#email-label").next(".col-sm-10").append("<span class='error-message'>Please provide a valid email</span>");
+      $emailLabel.parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-error has-feedback");
+      $emailLabel.next(".col-sm-10").children("span").remove();
+      $emailLabel.next(".col-sm-10").append("<span class='glyphicon glyphicon-remove form-control-feedback' aria-hidden='true'></span>");
+      $emailLabel.next(".col-sm-10").append("<span class='error-message'>Please provide a valid email</span>");
     }
   });
 
 
 // if Password if not longer than six characters throw an error
 
-$(".btn").click( function (event) {
+$btn.click( function (event) {
   "use strict";
-  if ($("#inputPassword").val().length < 6) {
+  if ($inputPassword.val().length < 6) {
     event.preventDefault();
-    $("#password-label").parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-error has-feedback");
-    $("#password-label").next(".col-sm-10").children("span").remove();
-    $("#password-label").next(".col-sm-10").append("<span class='glyphicon glyphicon-remove form-control-feedback' aria-hidden='true'></span>");
-    $("#password-label").next(".col-sm-10").append("<span class='error-message'>Please provide a password that is longer than five characters.</span>");
+    $passwordLabel.parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-error has-feedback");
+    $passwordLabel.next(".col-sm-10").children("span").remove();
+    $passwordLabel.next(".col-sm-10").append("<span class='glyphicon glyphicon-remove form-control-feedback' aria-hidden='true'></span>");
+    $passwordLabel.next(".col-sm-10").append("<span class='error-message'>Please provide a password that is longer than five characters.</span>");
   }
 });
 
 
-$("#inputPassword").blur( function () {
+$inputPassword.blur( function () {
   "use strict";
-  if ($("#inputPassword").val().length > 5) {
-    $("#password-label").parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-success has-feedback");
-    $("#password-label").next(".col-sm-10").children("span").remove();
-    $("#password-label").next(".col-sm-10").append("<span class='glyphicon glyphicon-ok form-control-feedback' aria-hidden='true'></span>");
+  if ($inputPassword.val().length > 5) {
+    $passwordLabel.parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-success has-feedback");
+    $passwordLabel.next(".col-sm-10").children("span").remove();
+    $passwordLabel.next(".col-sm-10").append("<span class='glyphicon glyphicon-ok form-control-feedback' aria-hidden='true'></span>");
   } else {
-    $("#password-label").parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-error has-feedback");
-    $("#password-label").next(".col-sm-10").children("span").remove();
-    $("#password-label").next(".col-sm-10").append("<span class='glyphicon glyphicon-remove form-control-feedback' aria-hidden='true'></span>");
-    $("#password-label").next(".col-sm-10").append("<span class='error-message'>Please provide a password that is longer than five characters.</span>");
+    $passwordLabel.parent(".form-group").removeClass("has-error has-success has-feedback").addClass("has-error has-feedback");
+    $passwordLabel.next(".col-sm-10").children("span").remove();
+    $passwordLabel.next(".col-sm-10").append("<span class='glyphicon glyphicon-remove form-control-feedback' aria-hidden='true'></span>");
+    $passwordLabel.next(".col-sm-10").append("<span class='error-message'>Please provide a password that is longer than five characters.</span>");
   }
 });
 
